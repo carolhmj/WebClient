79ef2113e6e46c684199c641eb11e2fe2e8f5f7d
CassioT <cassio.terceiro@gmail.com>
2014-06-16 12:20:31 -0300

    Mudança na classe UsuarioREST

--- src/com/clientews/acessows/UsuarioREST.java
+++ src/com/clientews/acessows/UsuarioREST.java
@@ -3,38 +3,33 @@ package com.clientews.acessows;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.clientews.entidade.Usuario;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonParser;
+
 
 public class UsuarioREST {
 
 	// EX: 200.153.36.140:8070
-	private static String URI = "http://192.168.1.20:8081/ServerWS/"; 
+	private static String URI = "http://192.168.1.100:8081/ServerWS/"; 
 
 	public List<Usuario> getUsuario() throws Exception {
 		
 		// Array de String que recebe o JSON do Web Service
 		String[] json = new WebService().get(URI + "usuario/listar");
-
+		JSONObject ujson = new JSONObject(json[1]);
+		JSONArray uarray = ujson.getJSONArray("Usuario");
 		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
 
 		if (json[0].equals("200")) {
 			
-			Gson gson = new Gson();
-
-			JsonParser parser = new JsonParser();
 
 			// Fazendo o parse do JSON para um JsonArray
-			JsonArray array = parser.parse(json[1]).getAsJsonArray();
-			System.out.println(array.toString());
-
-			for (int i = 0; i < array.size(); i++) {
-
-				// Adicionando na lista a posicao atual do JsonArray
-				usuarios.add(gson.fromJson(array.get(i), Usuario.class));
-			}
+			System.out.println(json[1].toString());
+			System.out.println(ujson.toString());
+			System.out.println(uarray.toString());
+			
 			// retornado a lista que consumiu do WS
 			for(Usuario u : usuarios){
 				System.out.println(u.getNomeUsuario());
